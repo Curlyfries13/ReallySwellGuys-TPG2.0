fa38867fc9181e12e7d4ca74f1e4c474390bed58
Jon Yocky <6563138+Curlyfries13@users.noreply.github.com>
2018-06-01 20:53:48 -0700

    modify test script to point to the correct location, comment out active part of the code

--- RSGTPG/send_sms.py
+++ RSGTPG/send_sms.py
@@ -10,15 +10,19 @@ from twilio.rest import Client
 
 config = configparser.ConfigParser()
 config.sections()
-config.read('example.ini')
+config.read('../config/example.ini')
+
+print(config.sections())
 
 account_sid = config['Twilio'].get('account_sid')
 auth_token = config['Twilio'].get('auth_token')
 
 client = Client(account_sid, auth_token)
 
+'''
 client.messages.create(
         to=config['PhoneNum'].get('twilio_num'),
         from_=config['PhoneNum'].get('customer_num'),
         body="Hello. Your container will be picked up at [Time] at [Address]."
         )
+'''
